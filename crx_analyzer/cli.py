9c926972098a09450fa13a7bacb33561aa171536
rileydakota <dakotariley2@gmail.com>
2025-01-15 10:18:24 -0500

    feat: cli shorthand args, version and analyze command

--- crx_analyzer/cli.py
+++ crx_analyzer/cli.py
@@ -1,4 +1,5 @@
 import click
+from importlib.metadata import version
 from rich.console import Console
 from rich.table import Table
 from .extension import Extension, Browser
@@ -6,22 +7,35 @@ from .risk import get_risk_report
 from .models import RiskLevel
 
 
-@click.command(name="crx-analysis")
-@click.option("--id", type=str, help="The ID of the extension to check", required=True)
+@click.group(name="crx-analyzer")
+def cli():
+    pass
+
+
+@cli.command(name="version")
+def get_version():
+    print(version("crx_analyzer"))
+
+
+@cli.command(name="analyze")
 @click.option(
+    "-i", "--id", type=str, help="The ID of the extension to check", required=True
+)
+@click.option(
+    "-b",
     "--browser",
     type=click.Choice(["chrome", "edge"]),
-    help="The browser the extension id is associated with (chrome or edge)",
+    help="The browser the extension id is associated with",
     required=True,
 )
 @click.option(
     "--output",
     type=click.Choice(["json", "pretty"]),
     default="pretty",
-    help="Output format (json or pretty)",
+    help="Output format",
     required=True,
 )
-def cli(id, browser, output):
+def analyze(id, browser, output):
     browser_enum = Browser(browser)
     extension = Extension(id, browser_enum)
     report = get_risk_report(extension)
@@ -67,13 +81,13 @@ def cli(id, browser, output):
 
             # Add both tables to grid
             grid.add_column(justify="left")
-            grid.add_column(justify="right")
+            grid.add_column(justify="center")
             grid.add_row(metadata_table, permissions_table)
 
             console = Console(force_terminal=True)
             console.print("\n")
             console.print(
-                f"[bold blue reverse]{browser} extension analysis for extesnion id {id}",
+                f"[bold blue reverse white]{browser} extension analysis for extesnion id {id}",
                 justify="center",
             )
             console.print("\n" * 3)
